6059f53f9dc968f96a4191286aa969e111cf5eb6
udommiracle2 <udommiracle2@gmail.com>
2026-09-18 02:46:40 +0200

    multi commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -33,6 +33,25 @@
 
 
 
+// import axios from "axios";
+
+// const API_URL =
+//   import.meta.env.VITE_API_URL ||
+//   "https://pharmacy-management-uj3w.onrender.com/api";
+
+// const api = axios.create({
+//   baseURL: API_URL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   withCredentials: false,
+// });
+
+// export default api;
+
+
+
+
 import axios from "axios";
 
 const API_URL =
@@ -46,5 +65,29 @@ const api = axios.create({
   },
   withCredentials: false,
 });
+
+// Attach the staff member's JWT to every outgoing request, if we have one
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('pharmacy_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+// If the token has expired or is invalid, bounce back to login
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('pharmacy_token');
+      localStorage.removeItem('pharmacy_user');
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default api;
